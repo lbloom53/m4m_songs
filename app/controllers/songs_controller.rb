6841634c0ced0d86e5fd31e7c570de7c576046b3
Lloyd Bloom <lbloom53@gmail.com>
2011-09-11 22:26:10 -0400

    Added a Songs controller

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,26 @@
+class SongsController < ApplicationController
+  def home
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
